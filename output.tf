898cf223e8ff95f857312b64a03d405417e70fb7
tacobayle <nicolas.bayle@gmail.com>
2021-11-26 14:02:07 +0000

    20211126_ubuntu_name_static

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "ubuntu_static_ips" {
+  value = var.ubuntu_ip4_addresses.*
+}
+
+output "ubuntu_dhcp_ips" {
+  value = vsphere_virtual_machine.ubuntu_dhcp.*.default_ip_address
+}
+
+output "ubuntu_username" {
+  value = var.ubuntu.username
+}
+
+output "ubuntu_password" {
+  value = random_string.ubuntu_password.result
+}
+
+output "ssh_private_key_path" {
+  value = "~/.ssh/${var.ssh_key.private_key_name}.pem"
+}
